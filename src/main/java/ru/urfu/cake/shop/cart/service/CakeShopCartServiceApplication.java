adccdd8c8fc833c48871d60c8d231bc0104a04cc
Alrons <titun766@gmail.com>
2026-02-21 15:50:45 +0500

    init

---
+++ src/main/java/ru/urfu/cake/shop/cart/service/CakeShopCartServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.cake.shop.cart.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeShopCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeShopCartServiceApplication.class, args);
+    }
+
+}
